052b5685df0ce56e7c5a0cbc52534876cad4fa32
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-21 13:30:32 +0100

    Make next stroke and display stick figure methods

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
-puts "Hangman started"
-
 class Game
   attr_reader :words, :secret_word
+  attr_accessor :lines
 
   def initialize
     @words = File.readlines("google-10000-english-no-swears.txt")
     @secret_word = select_secret_word(words)
+    @lines = Array.new(11, " ")
   end
 
   def select_secret_word(words)
@@ -15,8 +15,54 @@ class Game
 
     medium_length_words.compact.sample
   end
+
+  def play_game
+    wrong_guesses = 1
+    self.lines = make_stroke(wrong_guesses)
+    display_stick_figure(lines)
+  end
+
+  def make_stroke(wrong_guesses)
+    case wrong_guesses
+    when 1
+      lines[0] = "___"
+    when 2
+      lines[1] = "|"
+    when 3
+      lines[2] = "___________"
+    when 4
+      lines[3] = "/"
+    when 5
+      lines[4] = "|"
+    when 6
+      lines[5] = "(_)"
+    when 7
+      lines[6] = "|"
+    when 8
+      lines[7] = "/"
+    when 9
+      lines[8] = "\\"
+    when 10
+      lines[9] = "/"
+    when 11
+      lines[10] = "\\"
+    end
+    lines
+  end
+
+  def display_stick_figure(lines)
+    puts "    #{lines[2]}"
+    puts "   #{lines[1]} #{lines[3]}         #{lines[4]}"
+    puts "   #{lines[1]}#{lines[3]}         #{lines[5]}"
+    puts "   #{lines[1]}          #{lines[7]}#{lines[6]}#{lines[8]}"
+    puts "   #{lines[1]}           #{lines[6]}"
+    puts "   #{lines[1]}          #{lines[9]} #{lines[10]}"
+    puts "   #{lines[1]}"
+    puts "   #{lines[1]}"
+    puts "#{lines[0]}#{lines[1]}#{lines[0]}"
+    puts
+  end
 end
 
 game = Game.new
-
-puts game.secret_word
+game.play_game
