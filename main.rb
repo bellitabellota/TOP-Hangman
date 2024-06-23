84127ec821bc69c5a745bbc0f47acf2d7da8b503
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-23 15:36:00 +0100

    Move question to continue game into main.rb to not initialize new game if previous game is continued

--- main.rb
+++ main.rb
@@ -1,8 +1,18 @@
 require_relative "lib/game"
 require_relative "lib/player"
 
-require "yaml" ##??can also be put in main.rb??
+def continue_previous_game?
+  puts "Do you want to continue a previously saved game? (Y/N)"
+  loop do
+    player_decision = gets.chomp.downcase
 
+    break player_decision if player_decision == "y" || player_decision == "n"
+
+    puts "Invalid choice. Input must be Y or N."
+  end
+end
+
+player_decision = continue_previous_game?
 
 game = Game.new
 game.play_game(game)
